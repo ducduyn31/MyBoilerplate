342a463839527992b63382b73235c212bec480a5
Daniel Ng <ducduy.ababat@gmail.com>
2019-03-27 22:25:34 +0700

    version 0.5.2

--- config/modules/mongo.config.js
+++ config/modules/mongo.config.js
@@ -1,12 +1,12 @@
 module.exports = {
     auth: {
-        activate: false,
-        username: 'root',
-        password: 'secret',
+        activate: process.env.MONGO_AUTH,
+        username: process.env.MONGO_USER || 'root',
+        password: process.env.MONGO_PASSWORD || 'secret',
     },
-    host: 'localhost',
-    port: 27017,
+    host: process.env.MONGO_HOST || 'localhost',
+    port: process.env.MONGO_PORT || 27017,
     database: {
-        default: 'test',
+        default: process.env.MONGO_DB || 'oauth',
     }
 };
