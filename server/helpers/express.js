f1c2ad58d6ce2b4d9c5811c5fa5d498fb6be4095
Daniel Ng <ducduy.ababat@gmail.com>
2019-04-05 15:21:58 +0700

    remove bin

--- server/helpers/express.js
+++ server/helpers/express.js
@@ -4,6 +4,7 @@ const express = require('express');
 const compress = require('compression');
 const helmet = require('helmet');
 const cors = require('cors');
+const transformer = require('d-transformer').middleware;
 
 const app = express();
 
@@ -20,5 +21,20 @@ app.use(compress());
 app.use(helmet());
 
 app.use(cors());
+
+/*
+ *  Middlewares register
+ *
+ */
+
+app.use(transformer);
+
+const logger = require('../helpers/logger/logger.middleware');
+
+app.use(logger());
+
+app.use('/api', require('../../index.route'));
+
+require('../../server/helpers/errors/errorHandler');
 
 module.exports = app;
